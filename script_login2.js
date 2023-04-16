368d6a69c2a1fbaa89248473a247a3f581c7fffc
yash gupta <yashguupta9798@gmail.com>
2023-04-16 14:38:41 +0530

    initial

---
+++ script_login2.js
@@ -0,0 +1,29 @@
+// let password_store=document.getElementById('password1')
+// let a=password_store.innerText;
+// let int_password_store = parseInt(password_store, 10);
+// alert(a)
+
+
+
+
+//var form = document.getElementById('form');
+//var number = document.getElementById('password1');
+//form.onsubmit = function sign_up() {
+    //var variable = number.value;
+    //alert( variable );
+
+
+//};
+// alert(variable);
+
+// var form2 = document.getElementById('form2');
+// var number2 = document.getElementById('password2');
+// form2.onsubmit = function sign_in() {
+//     var variable2 = number2.value;
+//     if(variable2 == variable){
+//         alert("you have sucussfully logged in");
+// }
+//     else{
+//         alert("you have entered a wrong password");
+// }
+// };
