039ed84697e57a18ba2630fba8696c35319275e8
rajivranjanB <rajivranjandbg123@gmail.com>
2023-04-16 18:07:18 +0530

    rajeev

---
+++ hamberger.js
@@ -0,0 +1,14 @@
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav").classList.toggle("change");
+}
+
+// const Sun = document.getElementById("sun")
+
+// Sun.addEventListener('click',function(){
+    
+// })
+
+// function sunday(){
+//     document.getElementById("myform1").style.display="block";
+// }
